713454353ee0a3a8430d732ff5c9d3b191612c42
oonois <oonois@gmail.com>
2024-01-26 15:40:51 +0100

    project crm

---
+++ src/main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Layout from './components/Layout'
+import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
+import Index, {loader as clientesLoader} from './pages/Index'
+import ErrorPage from './components/ErrorPage'
+import EditarCliente, {loader as editClienteLoader, action as editClienteAction} from './components/EditarCliente'
+import {action as eliminarClienteAction} from './components/Cliente'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, 
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />,
+        action: nuevoClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editClienteLoader,
+        action: editClienteAction,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction
+      }
+    ]
+  },
+  
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
